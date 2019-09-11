761117e14ab2830eb7c8bbb6a58f6748de94a104
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-09-10 18:10:45 -0600

    started team data in team.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
+import{ useState, useEffect } from 'react'
+import { Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+import { team } from './team';
 
 function App() {
+  const [teamList, setTeamList] = useState([team])
+    useEffect(() => {
+      setTeamList(team);
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Home">
+      <h1>Hello World!</h1>
+      <Route exact path='/' render={props => <TeamList {...props} teamList={teamList} /> } />
+      <Route path='/team' render={props => <team {...props} teamList={teamList} /> } />
     </div>
   );
 }
---
+++ team-builder/src/team.js
@@ -0,0 +1,14 @@
+export const team = [
+    {
+      name: 'Loralie Flint',
+      email: 'loralie.flint0128@gmail.com',
+      role: 'Full-Stack Developer',
+      location: 'Salt Lake City UT'
+    },
+    {
+      name: 'Timothy Shores',
+      email: 'Email',
+      role: 'Team Lead at Lambda School',
+      location: 'IDK'
+    }
+  ]
